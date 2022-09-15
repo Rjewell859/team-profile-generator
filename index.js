02dcfd38e2a98242de6a3ef843489d05da5cc5c7
Headwall Forest <headwallforest27@gmail.com>
2022-09-15 16:08:42 -0400

    Manager creation is working

--- index.js
+++ index.js
@@ -1,39 +1,102 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
 
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
+const fileLocation = './dist/index.html'
 
 // prompts
 
-const questions = [{
-    type: 'input',
-    message: 'What is your team managers name?',
-    name: 'managerName',
-},
-{
-    type: 'input',
-    message: 'What is your team managers employee ID?',
-    name: 'managerId',
-},
-{
-    type: 'input',
-    message: 'What is your team managers email address?',
-    name: 'managerEmail',
-},
-{
-    type: 'input',
-    message: 'What is your team managers office number?',
-    name: 'managerOffice',
-},
-{
-    type: 'list',
-    message: 'Add an engineer, intern or finish building your team.',
-    name: 'newEmployee',
-    choices: ['Engineer', 'Intern', 'Done'],
-},
+const managerQuestions = [{
+        type: 'input',
+        message: 'What is your team managers name?',
+        name: 'managerName',
+    },
+    {
+        type: 'input',
+        message: 'What is your team managers employee ID?',
+        name: 'managerId',
+    },
+    {
+        type: 'input',
+        message: 'What is your team managers email address?',
+        name: 'managerEmail',
+    },
+    {
+        type: 'input',
+        message: 'What is your team managers office number?',
+        name: 'managerOffice',
+    },
+    {
+        type: 'list',
+        message: 'Add an engineer, intern or finish building your team.',
+        name: 'newEmployee',
+        choices: ['Engineer', 'Intern', 'Done'],
+    },
 ]
 
+var askQuestions = function () {
+    inquirer
+        .prompt(managerQuestions)
+        .then((data) =>
+            writeToFile(data)
+        )
+}
 
 
+
+var writeToFile = function (content) {
+
+    const manager = new Manager(content.managerName, content.managerId, content.managerEmail, content.managerOffice)
+
+    var managerHTML = `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+      <link rel="stylesheet" href="./assets/css/style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+      <title>Team Profile Generator</title>
+    </head>
+    <header>
+    <h1 class = card-header>Your Team Profile</h1>
+    </header>
+    <body>
+      <main id = 'teamLocation'>
+      <div class="card" style="width: 18rem;">
+  <div class="card-header">
+    <h3>${manager.getRole()}: ${manager.employeeName}</h3>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">ID: ${manager.id}</li>
+    <li class="list-group-item">Email: ${manager.email}</li>
+    <li class="list-group-item">Office Number: ${manager.office}</li>
+  </ul>
+</div>
+      </main>
+
+    </body>
+    
+    </html>
+    
+    `
+    fs.writeFile(fileLocation, managerHTML, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Manager succesfully created.")
+        }
+    });
+}
+
+function init() {
+    askQuestions()
+}
+
+init();
 // ask questions
 
 // write file function
@@ -55,6 +118,6 @@ const questions = [{
 //   <title>Document</title>
 // </head>
 // <body>
-  
+
 // </body>
 // </html>
