6a7c9c1be4b06f5f22343b4860108b84ca2b4e37
Headwall Forest <headwallforest27@gmail.com>
2022-09-15 21:58:45 -0400

    Committing functional icons, emails, github link.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+// Classes
+
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
 const fileLocation = './dist/index.html'
 
-// prompts
+var team = ``
+
+// Question Arrays
 
 const managerQuestions = [{
         type: 'input',
@@ -37,69 +41,226 @@ const managerQuestions = [{
     },
 ]
 
+const engineerQuestions = [{
+        type: 'input',
+        message: 'What is your engineers name?',
+        name: 'engineerName',
+    },
+    {
+        type: 'input',
+        message: 'What is your engineers employee ID?',
+        name: 'engineerId',
+    },
+    {
+        type: 'input',
+        message: 'What is your engineers email?',
+        name: 'engineerEmail',
+    },
+    {
+        type: 'input',
+        message: 'What is your engineers github account?',
+        name: 'engineerGithub',
+    },
+    {
+        type: 'list',
+        message: 'Add an engineer, intern or finish building your team.',
+        name: 'newEmployee',
+        choices: ['Engineer', 'Intern', 'Done'],
+    },
+]
+
+const internQuestions = [{
+        type: 'input',
+        message: 'What is your interns name?',
+        name: 'internName',
+    },
+    {
+        type: 'input',
+        message: 'What is your interns ID?',
+        name: 'internId',
+    },
+    {
+        type: 'input',
+        message: 'What is your interns email address?',
+        name: 'internEmail',
+    },
+    {
+        type: 'input',
+        message: 'What is your interns school name?',
+        name: 'internSchool',
+    },
+    {
+        type: 'list',
+        message: 'Add an engineer, intern or finish building your team.',
+        name: 'newEmployee',
+        choices: ['Engineer', 'Intern', 'Done'],
+    },
+]
+
+// Begin asking questions
+
 var askQuestions = function () {
     inquirer
         .prompt(managerQuestions)
         .then((data) =>
-            writeToFile(data)
+            createManager(data)
         )
 }
 
+var askEngQuestions = function () {
+    inquirer
+        .prompt(engineerQuestions)
+        .then((data) =>
+            createEngineer(data)
+        )
+}
 
+var askIntQuestions = function () {
+    inquirer
+        .prompt(internQuestions)
+        .then((data) =>
+            createIntern(data)
+        )
+}
 
-var writeToFile = function (content) {
+// Creates file and adds manager information to a card
 
-    const manager = new Manager(content.managerName, content.managerId, content.managerEmail, content.managerOffice)
-
-    var managerHTML = `<!DOCTYPE html>
+var writeToFile = function (team) {
+    var fileContent = `<!DOCTYPE html>
     <html lang="en">
     
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">  
-      <link rel="stylesheet" href="./assets/css/style.css">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <script src="https://kit.fontawesome.com/2054ad37bb.js" crossorigin="anonymous"></script>
       <title>Team Profile Generator</title>
     </head>
-    <header>
-    <h1 class = card-header>Your Team Profile</h1>
+    <header style = 'background-color:royalblue'>
+    <h1 class = 'card-header' style = 'text-align:center'>Your Team Profile</h1>
     </header>
-    <body>
-      <main id = 'teamLocation'>
-      <div class="card" style="width: 18rem;">
-  <div class="card-header">
-    <h3>${manager.getRole()}: ${manager.employeeName}</h3>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">ID: ${manager.id}</li>
-    <li class="list-group-item">Email: ${manager.email}</li>
-    <li class="list-group-item">Office Number: ${manager.office}</li>
-  </ul>
-</div>
+    <body style = 'background-color:rgb(185, 219, 248);'>
+      <main>
+      ${team}
       </main>
 
     </body>
     
-    </html>
-    
-    `
-    fs.writeFile(fileLocation, managerHTML, err => {
+    </html>`
+
+    fs.writeFile(fileLocation, fileContent, err => {
         if (err) {
             console.error(err);
         } else {
-            console.log("Manager succesfully created.")
+            console.log("File succesfully created.")
         }
     });
 }
+
+// Creating employees and cards, appending together to string together the HTML
+
+
+// todo: addicons!
+
+var createManager = function (content) {
+
+    const manager = new Manager(content.managerName, content.managerId, content.managerEmail, content.managerOffice)
+
+    var managerHTML = `
+    <div class="card" style="width: 18rem; height: 20rem; margin: 20px;display:inline-block;">
+    <div class="card-header" style = 'background-color:cornflowerblue'>
+    <h3 style='font-weight:bold;'>${manager.getRole()}: ${manager.employeeName}<i class="fas fa-suitcase-rolling"style="margin:5px;"></i></h3>
+  </div>
+  <div class="card-body" style = 'background-color:rgb(49, 177, 177); height:100%;'>
+  <ul class="list-group list-group-flush"style = 'background-color:aliceblue'>
+    <li class="list-group-item">ID: <span style = 'color:blue;'>${manager.id}</span</li>
+    <li class="list-group-item">Email: <a href = "mailto:${manager.email}">${manager.email}</a></li>
+    <li class="list-group-item">Office Number: <span style = 'color:blue;'>${manager.office}</span></li>
+  </ul>
+  </div>
+</div>`
+
+    team = managerHTML
+
+    if (content.newEmployee == 'Engineer') {
+        askEngQuestions()
+    } else if (content.newEmployee == 'Intern') {
+        askIntQuestions()
+    } else {
+        writeToFile(team)
+    }
+}
+
+var createEngineer = function (content) {
+
+    const engineer = new Engineer(content.engineerName, content.engineerId, content.engineerEmail, content.engineerGithub)
+
+    var engineerHTML = `
+    <div class="card" style="width: 18rem; height: 20rem; margin: 20px;display:inline-block;">
+    <div class="card-header" style = 'background-color:cornflowerblue'>
+    <h3>${engineer.getRole()}: ${engineer.employeeName}<i class="fas fa-wrench"style="margin:5px;"></i></h3>
+  </div>
+  <div class="card-body" style = 'background-color:rgb(49, 177, 177); height:100%;'>
+  <ul class="list-group list-group-flush" style = 'background-color:aliceblue'>
+    <li class="list-group-item">ID: <span style = 'color:blue;'>${engineer.id}</span></li>
+    <li class="list-group-item">Email: <a href = "mailto:${engineer.email}">${engineer.email}</a></span></li>
+    <li class="list-group-item">Github Account: <a href = 'https://github.com/${engineer.getGithub()}'>${engineer.getGithub()}</a></span></li>
+  </ul>
+  </div>
+</div>`
+
+    team = team + engineerHTML
+
+    if (content.newEmployee == 'Engineer') {
+        askEngQuestions()
+    } else if (content.newEmployee == 'Intern') {
+        askIntQuestions()
+    } else {
+        writeToFile(team)
+    }
+}
+
+// Append Intern to file
+
+var createIntern = function (content) {
+
+    const intern = new Intern(content.internName, content.internId, content.internEmail, content.internSchool)
+
+    var internHTML = `
+    <div class="card" style="width: 18rem; height: 20rem; margin: 20px;display:inline-block;">
+    <div class="card-header" style = 'background-color:cornflowerblue'>
+    <h3>${intern.getRole()}: ${intern.employeeName}<i class="fas fa-user-graduate"style="margin:5px;"></i></h3>
+  </div>
+  <div class="card-body" style = 'background-color:rgb(49, 177, 177); height:100%;'>
+  <ul class="list-group list-group-flush" style = 'background-color:aliceblue'>
+    <li class="list-group-item">ID: <span style = 'color:blue;'>${intern.id}</span></li>
+    <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+    <li class="list-group-item">School: <span style = 'color:blue;'>${intern.getSchool()}</span></li>
+  </ul>
+  </div>
+</div>`
+
+    team = team + internHTML
+
+    if (content.newEmployee == 'Engineer') {
+        askEngQuestions()
+    } else if (content.newEmployee == 'Intern') {
+        askIntQuestions()
+    } else {
+        writeToFile(team)
+    }
+}
+
+// Initialize File
 
 function init() {
     askQuestions()
 }
 
 init();
-// ask questions
 
-// write file function
 
 
 
@@ -112,9 +273,7 @@ init();
 // <!DOCTYPE html>
 // <html lang="en">
 // <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   
 //   <title>Document</title>
 // </head>
 // <body>
